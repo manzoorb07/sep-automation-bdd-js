894a675543a17b38ce658152c198e172d7365fa2
manzoorb07 <manzoorb07@gmail.com>
2026-04-15 22:51:54 -0400

    Updates

--- pages/BasePage.js
+++ pages/BasePage.js
@@ -1,22 +1,20 @@
-import { BrowserUtility } from '../utilities/BrowserUtility.js';
+import { BrowserUtility } from "../utilities/BrowserUtility.js";
 
 export class BasePage {
-
   /**
    * @param {import('playwright').Page} page
    */
   constructor(page) {
     this.page = page;
-
   }
-  
-  async login(){
-    const code = Buffer.from(`${process.env.SEP_USERNAME}:${process.env.SEP_PASSWORD}`).toString("base64");
-    await this.page.setExtraHTTPHeaders({Authorization: `Basic ${code}`,});
+
+  async login() {
+    const code = Buffer.from(
+      `${process.env.SEP_USERNAME}:${process.env.SEP_PASSWORD}`,
+    ).toString("base64");
+    await this.page.setExtraHTTPHeaders({ Authorization: `Basic ${code}` });
     await this.page.goto(process.env.SEP_QA_URL);
-    BrowserUtility.verify_title(this.page, 'Checkout | Cydeo');
-    await this.page.waitForLoadState("networkidle");   
-    
+    BrowserUtility.verify_title(this.page, "Checkout | Cydeo");
+    await this.page.waitForLoadState("networkidle");
   }
-
 }
